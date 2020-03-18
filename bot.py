1c04d19a5d354b4cc6cdd467bac19e6ef0365195
Jack Burgess <jack@jwburgess.co.uk>
2020-03-18 20:48:53 +0000

    Added memory command and help text

--- bot.py
+++ bot.py
@@ -10,19 +10,19 @@ GUILD = os.getenv('DISCORD_GUILD')
 bot = commands.Bot(command_prefix='!')
 
 ## BASIC TEXT COMMANDS ##
-@bot.command(name='hi')
+@bot.command(name='hi', help='Leo says hi')
 async def hi_leo(ctx):
     await ctx.send(
         f'Hi {ctx.author.mention}!'
     )
 
-@bot.command(name='sally')
+@bot.command(name='sally', help='Info about Sally the Seahorse')
 async def sally(ctx):
     await ctx.send(
         f'Sally is my Best Friend! We go on all our adventures together but we have to protect her, as other SSAGOers like to steal her! :frowning:'
     )
 
-@bot.command(name='git')
+@bot.command(name='git', help='Link to the Git Repo')
 async def git(ctx):
     await ctx.send(
         f'Here\'s the git repo that contains all my inner code. I may look like the best teddy lion you\'ve ever seen but theres a computer behind me!' +
@@ -30,16 +30,17 @@ async def git(ctx):
         'https://github.com/JWB-Git/LeoBot'
     )
 
-@bot.command(name="rally")
+@bot.command(name='rally', help='Info about viking rally')
 async def rally(ctx):
     await ctx.send(
         f'Did you know my friends from NUSSAGG and DUSSAG are hosting Viking Rally in November 2021! Please come join us for a weekend of great fun in the Toon '+
         'and surrounding areas. Its a canny place to be!'
     )
 
-@bot.command(name="adventure")
+@bot.command(name='adventure', help='Generates an adventure Leo wants to go on')
 async def adventure(ctx):
     mascots = ['Sally', 'Hamish', 'Conker', 'Harold', 'Gossy']
+
     locations = ['Hull', 'York', 'Glasgow', 'Edinburgh',
                 'Leicester', 'Bath', 'Durham', 'Sheffield',
                 'London', 'The Seaside', 'Outer Space', 'Leeds',
@@ -51,5 +52,20 @@ async def adventure(ctx):
     await ctx.send(
         f'I want to go to {location} with {mascot} on my next adventure! :airplane:'
     )
+
+@bot.command(name='memory', help='Leo says a random memory of his')
+async def memory(ctx):
+    memories = [
+        'I loved eating all the chocolate at Cadbury World when I went to Birmingham Chocolate Rally',
+        'I enjoyed exploring Bath on the Roman Rally Monopoly Run',
+        'I had great fun help run a Monopoly Run for City of Newcastle Scouts!',
+        'I really liked going to the North Tyneside coast, even if it was cold and windy!',
+        'I loved visiting Beamish and riding on the trams!',
+        'I had an amazing time at Northern Freshers in Hull, seeing Sally\'s Brothers and Sisters in the Deep'
+    ]
+
+    memory = random.choice(memories)
+
+    await ctx.send(memory)
     
 bot.run(TOKEN)
