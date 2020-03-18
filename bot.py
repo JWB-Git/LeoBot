e3b486aae876f1a4bf3a56e91ec547b6825f5bc6
Jack Burgess <jack@jwburgess.co.uk>
2020-03-18 18:31:27 +0000

    Updated bot.py to use bot api rather than message

--- bot.py
+++ bot.py
@@ -1,24 +1,32 @@
 import os
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+## BASIC TEXT COMMANDS ##
+@bot.command(name='hi')
+async def hi_leo(ctx):
+    await ctx.send(
+        f'Hi {ctx.author.mention}!'
+    )
 
-    if 'hi leo' in message.content.lower():
-        await message.channel.send(f'Hi {message.author.mention}!')
+@bot.command(name='sally')
+async def sally(ctx):
+    await ctx.send(
+        f'Sally is my Best Friend! We go on all our adventures together but we have to protect her, as other SSAGOers like to steal her! :frowning:'
+    )
 
-    if message.content.startswith('!sally'):
-        await message.channel.send(
-            f'Sally is my Best Friend! We go on all our adventures together but we have to protect her, as other SSAGOers like to steal her! :frowning:'
-        )
+@bot.command(name='git')
+async def git(ctx):
+    await ctx.send(
+        f'Here\'s the git repo that contains all my inner code. I may look like the best teddy lion you\'ve ever seen but theres a computer behind me!' +
+        '\n' +
+        'https://github.com/JWB-Git/LeoBot'
+    )
     
-client.run(TOKEN)
+bot.run(TOKEN)
