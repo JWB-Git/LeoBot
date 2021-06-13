9d96887afe22ff2b0086d3c2ac286c38ca17285c
trodaway <tim.rodaway@btinternet.com>
2021-06-13 12:50:08 +0100

    Hopefully make roar work again

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 from constants import *
+import math
+import random
 
 
 class Basic(commands.Cog):
@@ -26,21 +28,23 @@ class Basic(commands.Cog):
             roar = ""
 
             # Special Cases
-            if length == 69:
+            if length == "pi" or length == "🥧" or length == str(round(math.pi, len(length.split(".")[1]))):
+                roar = random.choice(["R🥧O🥧A🥧R", "```\n  R\nR   O\n  A\n```"])
+            elif int(length) == 69:
                 roar = ':rolling_eyes:'
-            elif length == 666:
+            elif int(length) == 666:
                 roar = ':japanese_ogre:'
-            elif length <= 0:
+            elif int(length) <= 0:
                 roar = 'How would I roar for zero or negative length!?'
-            elif length > 999:
+            elif int(length) > 999:
                 roar = 'I can\'t roar for that long!'
 
             # Normal Roar
             else:
                 roar = 'R'
-                for i in range(length):
+                for i in range(int(length)):
                     roar += 'o'
-                for i in range(length):
+                for i in range(int(length)):
                     roar += 'a'
                 roar += 'r'
 
